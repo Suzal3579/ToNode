004e33ac1527e25320626af8de8cf951e3727902
Suzal3579 <pe.messh@gmail.com>
2017-06-12 19:10:28 +0545

    initial configuration made

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+
+router.post("/insert", (req, res, next) => {
+
+});
+
+module.exports = router;
