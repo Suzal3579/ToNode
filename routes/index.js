220865833e943716aa841410a931f989a9c7458a
Suzal3579 <pe.messh@gmail.com>
2017-06-13 13:07:12 +0545

    update from left for design ...other all working

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,59 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const mongoose = require("mongoose");
+mongoose.connect("localhost:27017/test");
 
+let ItemSchema = mongoose.Schema;
+let itemDataSchema = new ItemSchema({
+    item: String
+}, { collection: "item-data" });
+
+let itemData = mongoose.model("itemData", itemDataSchema);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.redirect('/getdata');
 });
 
 router.post("/insert", (req, res, next) => {
-    let lists = {
-
+    let itemList = {
+        item: req.body.item
     };
+    let itemInstance = new itemData(itemList);
+    itemInstance.save();
+    res.redirect("/getdata");
 });
+
+router.get("/getdata", (req, res, next) => {
+    itemData.find({}, (err, docs) => {
+        console.log({ itemListData: docs });
+        res.render("index", { itemListData: docs });
+    })
+});
+
+router.get("/update/:id", (req, res, next) => {
+    let id = req.params.id;
+    res.render("update", { id: id });
+});
+
+router.post("/updateItem/:id", (req, res, next) => {
+    let id = req.params.id;
+    itemData.findById(id, (error, docs) => {
+        if (error) {
+            res.render("error", { error: error });
+        } else {
+            docs.item = req.body.item;
+            docs.save();
+            res.redirect("/getdata");
+        }
+    });
+});
+
+router.get("/delete/:id", (req, res, next) => {
+    let id = req.params.id;
+    itemData.findByIdAndRemove(id).exec();
+    res.redirect("/getdata");
+});
+
 
 module.exports = router;
